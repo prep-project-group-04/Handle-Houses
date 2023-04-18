7712e1d0112b01c349180fa3da1cad9c3b417427
qaisdw <qaisdw96@hotmail.com>
2023-04-18 12:03:55 +0000

    test

--- src/Components/ForgPass/ForgPass.js
+++ src/Components/ForgPass/ForgPass.js
@@ -2,19 +2,18 @@ import {Link} from 'react-router-dom';
 // @ts-ignore
 // import axios from 'axios';
 import { useState } from 'react';
-// @ts-ignore
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// @ts-ignore
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import "./ForgPass.css";
 
 
 
 export default function ForgetPass() {
-    const [email, setEmail] = useState('');
+    const [email, setEmail] = useState([]);
     
     async function checkEmail(){
-       
+      let url =`/email`;
+      const response = await fetch(url);
+      const jsonData = await response.json();
+      console.log(jsonData);
     }
     
     
@@ -29,8 +28,7 @@ export default function ForgetPass() {
         <form action="" method="post">
           <h1>Forget Password</h1>
           <div className='iconEmail'>
-            <span><FontAwesomeIcon icon={faEnvelope} /></span>
-            <input type="email" id="email1" name="email" placeholder="Please enter your Email" value={email}
+            <input type="email" id="email" name="email" placeholder="Please enter your Email" value={email}
               onChange={(event) => setEmail(event.target.value)}  required />
           </div>
           <Link to="/reset-password">
--- src/Components/ForgPass/Restpass.js
+++ src/Components/ForgPass/Restpass.js
@@ -1,13 +1,7 @@
 import React, { useState } from "react";
 import {  Link } from "react-router-dom";
-// import { Redirect } from 'react-router';
 // @ts-ignore
 import { redirect } from "react-router-dom";
-
-// @ts-ignore
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// @ts-ignore
-import { faLock } from "@fortawesome/free-solid-svg-icons";
 // @ts-ignore
 import axios from "axios";
 import "./ForgPass.css";
@@ -44,13 +38,11 @@ export default function PasswordReset(props) {
         <form action="" method="post">
           <h1>Forget Password</h1>
           <div className='iconEmail'>
-            <span><FontAwesomeIcon icon={faLock} /></span>
-            <input type="password" id="email1"  placeholder="Please Enter your Password" value={password}
+            <input type="password" id="email"  placeholder="Please Enter your Password" value={password}
             onChange={(event) => setPassword(event.target.value)} required />
           </div>
           <div className='iconEmail'>
-            <span><FontAwesomeIcon icon={faLock} /></span>
-            <input type="password" id="email1" placeholder="Please Conform your Password"  value={confirmPassword}
+            <input type="password" id="email" placeholder="Please Conform your Password"  value={confirmPassword}
             onChange={(event) => setConfirmPassword(event.target.value)} required />
           </div>
           <button type="submit" onClick={(e)=>{handleSubmit(e)}}>Recover</button>
